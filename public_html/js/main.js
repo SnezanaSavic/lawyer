92bb3d4c8b0322529fd022fcadec2ea39e935230
Snezana <snezanadjorovic94@gmail.com>
2023-02-02 23:41:09 +0100

    Services page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,45 +1,50 @@
 
-$(document).ready(function () {
-
-    //animation
-
+$(document).ready(function(){
+    
+    //ANIMATION
     function animation() {
-        let windowHeight = $(window).height();
+        let  windowHeight = $(window).height();
         let scroll = $(window).scrollTop();
         $('.animation').each(function () {
             let position = $(this).offset().top;
             let animationName = $(this).attr('data-animation');
             let delay = $(this).attr('data-delay');
+
             if (position < windowHeight + scroll - 100) {
                 $(this).addClass(animationName);
                 $(this).css('animation-delay', delay);
             }
         });
     }
+
+
     $(window).scroll(function () {
         animation();
     });
     animation();
-    //team slider
 
-    if ($('.team-members-slider'.length > 0)) {
-
+    // team-slider
+    
+    if($('.team-members-slider').length > 0){
         $(".team-members-slider").owlCarousel({
+            
             loop:true,
             autoplay:true,
             
             responsive:{
-             0:{
-                 items:1,
-                 margin:0
-                
-             },
-             992:{
-                 items:2,
-                 margin:30
-             }
+                0:{
+                    items:1,
+                    margin:0
+                },
+                992:{
+                    items:2,
+                    margin:30
+                }
             }
+            
         });
     }
-
+    
+    
+    
 });
