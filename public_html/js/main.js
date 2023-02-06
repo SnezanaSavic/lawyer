92b04526a0afa65b7672a6d552bc5cf55bfeae34
Snezana <snezanadjorovic94@gmail.com>
2023-02-06 13:30:10 +0100

    Contact page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,6 +1,6 @@
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     //ANIMATION
     function animation() {
         let  windowHeight = $(window).height();
@@ -24,27 +24,73 @@ $(document).ready(function(){
     animation();
 
     // team-slider
-    
-    if($('.team-members-slider').length > 0){
+
+    if ($('.team-members-slider').length > 0) {
         $(".team-members-slider").owlCarousel({
-            
-            loop:true,
-            autoplay:true,
-            
-            responsive:{
-                0:{
-                    items:1,
-                    margin:0
+
+            loop: true,
+            autoplay: true,
+
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 0
                 },
-                992:{
-                    items:2,
-                    margin:30
+                992: {
+                    items: 2,
+                    margin: 30
                 }
             }
-            
+
         });
     }
-    
-    
-    
+
+    //form validator
+    //validate form
+    if ($('.contact-form').length > 0) {
+        $('.contact-form').validate({
+            highlight: function (element) {
+
+                $(element).addClass('is-invalid').removeClass('is-valid');
+            },
+            unhighlight: function (element) {
+
+                $(element).addClass('is-valid').removeClass('is-invalid');
+            },
+            rules: {
+                name: {
+                    required: true
+
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+
+                message: {
+                    required: true
+                }
+            },
+            messages: {
+                name: {
+                    required: 'The Name* field is required'
+                },
+                email: {
+                    required: 'The Email* field is required',
+                    email: 'Please provide a valid email address'
+                },
+                message: {
+                    required: 'The Message* field is required'
+                }
+            },
+            errorElementa: 'p',
+            errorPlacement: function (error, element) {
+                error.appendTo(element.closest(".form-group").find(".error-msg"));
+            }
+        });
+
+
+    }
+    ;
+
 });
